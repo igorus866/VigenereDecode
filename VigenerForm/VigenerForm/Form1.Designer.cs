a67cde82dcc383c0ec7bc9427956329c22f96c55
igorus866 <81354763+igorus866@users.noreply.github.com>
2021-03-26 19:14:01 +0300

    Add files via upload

---
+++ VigenerForm/VigenerForm/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace VigenerForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Encrypt = new System.Windows.Forms.Button();
+            this.Decrypt = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Encrypt
+            // 
+            this.Encrypt.Location = new System.Drawing.Point(264, 253);
+            this.Encrypt.Name = "Encrypt";
+            this.Encrypt.Size = new System.Drawing.Size(104, 23);
+            this.Encrypt.TabIndex = 1;
+            this.Encrypt.Text = "Зашифровать";
+            this.Encrypt.UseVisualStyleBackColor = true;
+            this.Encrypt.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Decrypt
+            // 
+            this.Decrypt.Location = new System.Drawing.Point(264, 288);
+            this.Decrypt.Name = "Decrypt";
+            this.Decrypt.Size = new System.Drawing.Size(104, 23);
+            this.Decrypt.TabIndex = 2;
+            this.Decrypt.Text = "Разшифровать";
+            this.Decrypt.UseVisualStyleBackColor = true;
+            this.Decrypt.Click += new System.EventHandler(this.Decrypt_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(51, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(711, 180);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(51, 317);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(346, 307);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(426, 317);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(336, 307);
+            this.richTextBox3.TabIndex = 12;
+            this.richTextBox3.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(51, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 32);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Ключ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(51, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 32);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Исходник";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(442, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 32);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 636);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Decrypt);
+            this.Controls.Add(this.Encrypt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Encrypt;
+        private System.Windows.Forms.Button Decrypt;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ VigenerForm/VigenerForm/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VigenerForm
+{
+    public partial class Form1 : Form
+    {
+        string em;
+        string dm;
+        string k;
+        string m;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Encrypt
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n;
+            int d;
+            int g, j;
+            int t = 0;
+            char[] message = m.ToCharArray();
+            char[] key = k.ToCharArray();
+            char[] alphabet = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', '_' };
+            for (int i=0; i < message.Length; i++)
+            {
+                for (j=0; j < alphabet.Length; j++)
+                {
+                    if (message[i] == alphabet[j])
+                    {
+                        break;
+                    }
+                }
+                if (j != alphabet.Length)
+                {
+                    n = j;
+                    if (t>key.Length - 1)
+                    {
+                        t = 0;
+                    }
+                    for (g = 0; g < alphabet.Length; g++)
+                    {
+                        if (key[t] == alphabet[g])
+                        {
+                            break;
+                        }
+                    }
+                    t++;
+                    if (g != alphabet.Length)
+                    {
+                        d = n + g;
+                    }
+                    else
+                    {
+                        d = n;
+                    }
+                    if (d > alphabet.Length - 1)
+                    {
+                        d = d - alphabet.Length;
+                    }
+                    message[i] = alphabet[d];
+                }
+               
+            }
+            em = new string(message);
+            richTextBox3.Text = em;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            k = Convert.ToString(richTextBox1.Text);
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            m = Convert.ToString(richTextBox2.Text);
+        }
+
+        private void Decrypt_Click(object sender, EventArgs e)
+        {
+            int n;
+            int d;
+            int g, j;
+            int t = 0;
+            char[] message = m.ToCharArray();
+            char[] key = k.ToCharArray();
+            char[] alphabet = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', '_' };
+            for (int i = 0; i < message.Length; i++)
+            {
+                for (j = 0; j < alphabet.Length; j++)
+                {
+                    if (message[i] == alphabet[j])
+                    {
+                        break;
+                    }
+                }
+                if (j != alphabet.Length)
+                {
+                    n = j;
+                    if (t > key.Length - 1)
+                    {
+                        t = 0;
+                    }
+                    for (g = 0; g < alphabet.Length; g++)
+                    {
+                        if (key[t] == alphabet[g])
+                        {
+                            break;
+                        }
+                    }
+                    t++;
+                    if (g != alphabet.Length)
+                    {
+                        d = n - g + alphabet.Length;
+                    }
+                    else
+                    {
+                        d = n;
+                    }
+                    if (d > alphabet.Length - 1)
+                    {
+                        d = d - alphabet.Length;
+                    }
+                    message[i] = alphabet[d];
+                }
+
+            }
+            dm = new string(message);
+            richTextBox3.Text = dm;
+        }
+    }
+}
